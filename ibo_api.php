31c036a26da2d13b831492286d55d98402c8cef4
imperiumtv84-netizen <imperiumtv84@gmail.com>
2026-03-01 15:02:56 -0300

    Update ibo_api.php

--- ibo_api.php
+++ ibo_api.php
@@ -1,1 +1,23 @@
 
+<?php
+include 'config.php';
+
+function buscarDadosIbo() {
+    global $cookie_file;
+    $url = "https://iboplayer.com/device/login";
+    
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file); // Salva o cookie inicial
+    $html = curl_exec($ch);
+    
+    // Lógica para extrair a URL da imagem do captcha e o Token CSRF do HTML
+    // Usaremos expressões regulares (preg_match) aqui depois
+    return $html; 
+}
+
+function enviarParaIbo($mac, $key, $dns, $user, $pass, $captcha) {
+    global $cookie_file;
+    // Aqui faremos o POST simulando o clique do botão "Login" no site do IBO
+    // Usando o $cookie_file para manter a mesma sessão do captcha
+}
