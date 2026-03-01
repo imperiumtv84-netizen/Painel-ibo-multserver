706d4051a08a9bee870e852f75574e1d40ea89d9
imperiumtv84-netizen <imperiumtv84@gmail.com>
2026-03-01 15:16:03 -0300

    Update exibir_captcha.php

--- exibir_captcha.php
+++ exibir_captcha.php
@@ -1,1 +1,11 @@
 
+<?php
+include 'ibo_api.php';
+$url = carregarCaptcha();
+
+header('Content-Type: image/png');
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file); // Usa o mesmo cookie da sessão!
+curl_exec($ch);
+curl_close($ch);
+?>
