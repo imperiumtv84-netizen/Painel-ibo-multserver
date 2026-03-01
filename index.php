8662a8345822894208f0cf2b99f02c70e5d45591
imperiumtv84-netizen <imperiumtv84@gmail.com>
2026-03-01 15:04:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 
+<form action="processar.php" method="POST">
+    <input type="text" name="mac" placeholder="Seu MAC Address">
+    <input type="text" name="key" placeholder="Sua Device Key">
+    
+    <div class="captcha-box">
+        <img src="exibir_captcha.php" alt="Captcha IBO">
+        <input type="text" name="captcha_code" placeholder="Digite o código acima">
+    </div>
+    
+    <button type="submit">ATIVAR TODOS OS SERVIDORES</button>
+</form>
