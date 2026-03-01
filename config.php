67289fbec574b7850c45605273d0e165e0e77200
imperiumtv84-netizen <imperiumtv84@gmail.com>
2026-03-01 15:02:12 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,11 @@
 
+<?php
+// Suas DNS de Backup
+$meus_servidores = [
+    "http://dns-principal.com:8080",
+    "http://dns-backup-01.com:8080",
+    "http://dns-backup-02.com:8080"
+];
+
+// Caminho do arquivo de sessão (importante para o captcha funcionar)
+$cookie_file = __DIR__ . '/session_cookie.txt';
